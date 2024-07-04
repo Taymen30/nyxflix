978bd8324db2629aa23d29a32ece97c84daea9bf
Taymen30 <taymenv@icloud.com>
2024-07-04 20:10:29 +0800

    10 wide

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Search from "./pages/Search";
 import Genre from "./pages/Genre";
 import Bookmarks from "./pages/Bookmarks";
 import Person from "./pages/Person";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
@@ -15,34 +16,37 @@ export default function App() {
   const [lastFetchedMoviePage, setLastFetchedMoviePage] = useState(0);
   const [lastFetchedTvPage, setLastFetchedTvPage] = useState(0);
   return (
-    <Router>
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={
-            <Home
-              movies={movies}
-              setMovies={setMovies}
-              tvShows={tvShows}
-              setTvShows={setTvShows}
-              currentTvPage={currentTvPage}
-              setCurrentTvPage={setCurrentTvPage}
-              currentMoviePage={currentMoviePage}
-              setCurrentMoviePage={setCurrentMoviePage}
-              lastFetchedMoviePage={lastFetchedMoviePage}
-              setLastFetchedMoviePage={setLastFetchedMoviePage}
-              lastFetchedTvPage={lastFetchedTvPage}
-              setLastFetchedTvPage={setLastFetchedTvPage}
-            />
-          }
-        />
-        <Route exact path="/:type/:id" element={<MediaDetails />} />
-        <Route exact path="/search" element={<Search />} />
-        <Route exact path="/genre/:genre" element={<Genre />} />
-        <Route exact path="/bookmarks" element={<Bookmarks />} />
-        <Route exact path="/person/:id" element={<Person />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Home
+                movies={movies}
+                setMovies={setMovies}
+                tvShows={tvShows}
+                setTvShows={setTvShows}
+                currentTvPage={currentTvPage}
+                setCurrentTvPage={setCurrentTvPage}
+                currentMoviePage={currentMoviePage}
+                setCurrentMoviePage={setCurrentMoviePage}
+                lastFetchedMoviePage={lastFetchedMoviePage}
+                setLastFetchedMoviePage={setLastFetchedMoviePage}
+                lastFetchedTvPage={lastFetchedTvPage}
+                setLastFetchedTvPage={setLastFetchedTvPage}
+              />
+            }
+          />
+          <Route exact path="/:type/:id" element={<MediaDetails />} />
+          <Route exact path="/search" element={<Search />} />
+          <Route exact path="/genre/:genre" element={<Genre />} />
+          <Route exact path="/bookmarks" element={<Bookmarks />} />
+          <Route exact path="/person/:id" element={<Person />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
