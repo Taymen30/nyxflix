066351fb002add76fd8dbe0f165db7a3c580375b
Taymen30 <taymenv@icloud.com>
2024-07-11 18:41:11 +0800

    forgot the home button sizing

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -9,7 +9,7 @@ export default function HomeButton() {
       {!isHomePage && (
         <Link to={"/"}>
           <img
-            className="w-[40px] h-[40px] hover:opacity-70 transition-opacity duration-300"
+            className="sm:w-[40px] sm:h-[40px] w-[30px] h-[30px]  hover:opacity-70 transition-opacity duration-300"
             src="/house-home.512x512.png"
             alt=""
           />
