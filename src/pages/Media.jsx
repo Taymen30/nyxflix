51cda17f54faf2dcaafd260969a90f079a8e0daa
Taymen30 <taymenv@icloud.com>
2024-08-04 18:49:19 +0800

    resize imgs

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -174,7 +174,9 @@ export default function MediaDetails({
 
         {isGamerMode && (
           <div className="bg-black bg-opacity-30 rounded relative w-2/3">
-            <p className="text-center p-1">Season: {currentEpisode.season}</p>
+            <p className="text-center text-xl p-1">
+              Season: {currentEpisode.season}
+            </p>
             <div
               ref={episodeCarouselRef}
               className="overflow-x-auto m-2 whitespace-nowrap "
@@ -188,23 +190,37 @@ export default function MediaDetails({
                     }));
                   }}
                   key={episode.episode_number}
-                  className={`inline-block w-64 p-2 rounded-lg mr-4 hover:cursor-pointer transition-all duration-300 hover:brightness-125 ${
+                  className={`inline-block w-48 md:w-64 p-2 rounded-lg mr-4 hover:cursor-pointer transition-all duration-300 hover:brightness-125 ${
                     currentEpisode.episode === episode.episode_number
                       ? "bg-yellow-300 text-black"
                       : "bg-black bg-opacity-30"
                   }`}
                 >
-                  {episode.still_path && (
-                    <img
-                      src={`https://image.tmdb.org/t/p/w500/${episode.still_path}`}
-                      alt={episode.name}
-                      className="w-full h-auto mb-2 rounded"
-                    />
-                  )}
-                  <p className="text-sm font-bold">{episode.name}</p>
-                  <p className="text-xs">
-                    {new Date(episode.air_date).toLocaleDateString("en-AU")}
-                  </p>
+                  <div className="relative">
+                    {episode.still_path && (
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${episode.still_path}`}
+                        alt={episode.name}
+                        className="w-full h-auto mb-2 rounded"
+                      />
+                    )}
+                    <div className="absolute top-2 right-2 w-5 h-5 border border-white bg-black bg-opacity-70 rounded-full flex items-center justify-center">
+                      <span className="text-white text-xs font-bold">
+                        {episode.episode_number}
+                      </span>
+                    </div>
+                  </div>
+                  <section className="m-1">
+                    <p className="text-sm font-bold">{episode.name}</p>
+                    <div className="flex justify-between mt-1">
+                      <p className="text-xs">
+                        {new Date(episode.air_date).toLocaleDateString("en-AU")}
+                      </p>
+                      {episode.runtime && (
+                        <p className="text-xs">{episode.runtime} min</p>
+                      )}
+                    </div>
+                  </section>
                 </div>
               ))}
             </div>
