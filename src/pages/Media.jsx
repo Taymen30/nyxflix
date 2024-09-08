0c30818ec740acbefe279f3a77a106b514025c9a
Taymen30 <taymenv@icloud.com>
2024-09-08 18:38:42 +0800

    desciption issues

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -172,10 +172,9 @@ export default function MediaDetails({
           />
         </section>
 
-        {!isGamerMode ||
-          (type !== "tvshow" && (
-            <p className="text-xs md:text-lg w-2/3">{mediaDetails.overview}</p>
-          ))}
+        {isGamerMode && type !== "tvshow" && (
+          <p className="text-xs md:text-lg w-2/3">{mediaDetails.overview}</p>
+        )}
 
         {isGamerMode && type === "tvshow" && (
           <div className="bg-black bg-opacity-30 rounded relative w-2/3">
