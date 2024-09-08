f4d599bc20f1676a74711e3f26b13c3dcfa3091d
Taymen30 <taymenv@icloud.com>
2024-09-08 18:30:26 +0800

    vidsrccc

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -44,8 +44,8 @@ export default function Player({
     iframe.setAttribute("allowFullScreen", true);
     iframe.src = gamer
       ? type === "movie"
-        ? `https://multiembed.mov/directstream.php?video_id=${imdb_id}`
-        : `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}`
+        ? `https://vidsrc.cc/v3/embed/movie/${imdb_id}`
+        : `https://vidsrc.cc/v3/embed/tv/${id}/${season}/${episode}`
       : `https://www.youtube.com/embed/${trailerId}`;
 
     document.getElementById("player-container").appendChild(iframe);
